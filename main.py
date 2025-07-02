9cbbc470e34705be2886a9123e336965153442f0
Axel_Fouley <130937606+axel-fouley@users.noreply.github.com>
2025-07-02 22:11:24 +0200

    Update main.py 2

--- main.py
+++ main.py
@@ -65,3 +65,8 @@ def home():
 
 if __name__ == "__main__":
     app.run()
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render задаёт PORT в окружении
+    app.run(host="0.0.0.0", port=port)
